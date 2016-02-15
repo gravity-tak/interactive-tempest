f908a59518d9749719ba5fdddc85b2c3d667ba22
Alex Kang <akang@vmware.com>
2016-02-14 21:33:50 -0800

    init services clients folder

--- itempest/tools/build_mtz.py
+++ itempest/tools/build_mtz.py
@@ -1,16 +1,26 @@
-
+import exceptions
 import netaddr
 
-from itempest.commands import cmd_neutron_u1 as UQ
 from tempest.common import waiters
 from tempest_lib.common.utils import data_utils
 import lib_networks as LN
 
 
-# scope_id_list=['vdnscope-1', 'vdnscope-2', 'vdnscope-3']
-# mtz = build_mtz.setup_mtz_simple(sun, 'mtz-s', scope_id_list=scop_id_list)
+# scope_id_list=[None, 'vdnscope-1', 'vdnscope-2', 'vdnscope-3']
+# mtz = build_mtz.setup_mtz_simple(sun, 'mtz-s',
+#       for_tenant=mars, scope_id_list=scop_id_list,
+#       dns_nameservers=['10.34.35.11'], dns_search_domain='vmware.com')
 def setup_mtz_simple(cmgr, x_name, **kwargs):
     x_name = x_name or data_utils.rand_name('mtz-i')
+    tenant_cmgr = kwargs.pop('for_tenant', None)
+    if tenant_cmgr:
+        tenant_id = tenant_cmgr.manager.credentials.tenant_id
+    else:
+        tenant_cmgr = cmgr
+        tenant_id = kwargs.pop('tenant_id', None)
+        if tenant_id:
+            msg = "tenant_id not supported, use for_tenant=tenant_cmgr"
+            raise exceptions.NotImplementedError(msg)
     scope_id_list = kwargs.pop('scope_id_list', [])
     mtz_ip = netaddr.IPNetwork(kwargs.pop('cidr', '10.199.1.0/24'))
     mask_bits = kwargs.pop('mask_bits', (mtz_ip.prefixlen + 3))
@@ -20,25 +30,46 @@ def setup_mtz_simple(cmgr, x_name, **kwargs):
         subnet_cidr = str(cidr_list[ix])
         name = x_name + ("-%d" % (ix + 1))
         network_subnet = create_mtz_networks(cmgr, scope_id, subnet_cidr,
-                                             name=name)
+                                             name=name, tenant_id=tenant_id,
+                                             **kwargs)
         net_list.append(network_subnet)
-    router = LN.create_router_and_add_interfaces(cmgr, x_name+"-router",
-                                              net_list)
-    sg = UQ.create_security_group_loginable(cmgr.manager, x_name)
-    servers = {}
+    # server_create does not accept tenant_id, always use tenant_cmgr
+    router = LN.create_router_and_add_interfaces(tenant_cmgr,
+                                                 x_name + "-router",
+                                                 net_list,
+                                                 tenant_id=tenant_id)
+    sg = LN.create_security_group_loginable(tenant_cmgr, x_name,
+                                            tenant_id=tenant_id)
+    security_group_id = sg['id']
+    net_id_servers = {}
     for ix, (network, subnet) in enumerate(net_list):
         net_id = network['id']
         vm = LN.create_server_on_network(
-            cmgr, net_id, security_group_name_or_id=sg['name'],
-            server_name=x_name+("-%d" % ix))
-        servers[net_id] = dict(server=vm,
+            tenant_cmgr, net_id,
+            security_group_name_or_id=security_group_id,
+            server_name=network['name'])
+        net_id_servers[net_id] = dict(server=vm,
                                network=network, subnet=subnet)
-    return (router, servers, sg)
+    return (router, net_id_servers, sg)
 
 
-def teardown_mtz_simple(cmgr, router, servers, sg):
+def wait_for_servers_active(cmgr, net_id_servers):
+    server_id_list = [v['server']['id'] for k, v in net_id_servers.items()]
+    for server_id in server_id_list:
+        waiters.wait_for_server_status(
+            cmgr.manager.servers_client, server_id, 'ACTIVE')
+
+
+def wait_for_servers_terminated(cmgr, net_id_servers):
+    server_id_list = [v['server']['id'] for k, v in net_id_servers.items()]
+    for server_id in server_id_list:
+        waiters.wait_for_server_termination(
+            cmgr.manager.servers_client, server_id)
+
+
+def teardown_mtz_simple(cmgr, router, net_id_servers, sg):
     server_list = []
-    for net_id, server in servers.items():
+    for net_id, server in net_id_servers.items():
         server_id = server['server']['id']
         cmgr.nova('server-delete', server_id)
         server_list.append(server_id)
@@ -46,24 +77,24 @@ def teardown_mtz_simple(cmgr, router, servers, sg):
     for server_id in server_list:
         waiters.wait_for_server_termination(cmgr.manager.servers_client,
                                             server_id)
-    UQ.delete_this_router(cmgr.manager, router)
+    LN.delete_this_router(cmgr.manager, router)
     cmgr.qsvc('security-group-delete', sg['id'])
 
 
-def create_mtz_networks(cmgr, scope_id, cidr, **kwargs):
-    network_name = (kwargs.pop('name', None) or
-                    data_utils.rand_name('mtz-n'))
+def create_mtz_networks(cmgr, scope_id, cidr, name=None, **kwargs):
+    network_name = name or data_utils.rand_name('mtz-n')
     tenant_id = kwargs.get('tenant_id', None)
-    network_cfg = {
-        'name': network_name,
-        'provider:network_type': 'vxlan',
-        'provider:physical_network': scope_id,
-    }
+    if type(scope_id) in (str, unicode):
+        network_cfg = {
+            'provider:network_type': 'vxlan',
+            'provider:physical_network': scope_id,
+        }
+    else:
+        network_cfg = {}
     if tenant_id:
         network_cfg['tenant_id'] = tenant_id
-    network = cmgr.qsvc('net-create', **network_cfg)
+    network = cmgr.qsvc('net-create', network_name, **network_cfg)
     subnet = cmgr.qsvc('subnet-create', network['id'], cidr,
                        name=network_name, **kwargs)
     network = cmgr.qsvc('net-show', network['id'])
     return (network, subnet)
-
--- itempest/tools/lib_networks.py
+++ itempest/tools/lib_networks.py
@@ -1,9 +1,46 @@
+import netaddr
+
 from tempest_lib.common.utils import data_utils
+from tempest.common import waiters
+from tempest.common.utils.linux import remote_client
 
 
-def create_router_and_add_interfaces(cmgr, name, net_list):
+def create_security_group_loginable(cmgr, name, **kwargs):
+    filters = dict(name=name)
+    tenant_id = kwargs.pop('tenant_id', None)
+    if tenant_id:
+        filters['tenant_id'] = tenant_id
+    try:
+        sg = cmgr.qsvc('security-group-list', **filters)[0]
+    except Exception:
+        sg = cmgr.qsvc('security-group-create', name, tenant_id=tenant_id)
+        create_security_group_ssh_rule(cmgr, sg['id'], tenant_id=tenant_id)
+        create_security_group_icmp_rule(cmgr, sg['id'], tenant_id=tenant_id)
+    return cmgr.qsvc('security-group-show', sg['id'])
+
+
+def create_security_group_ssh_rule(cmgr, security_group_id, tenant_id=None):
+    ssh_rule = dict(direction='ingress',
+                    ethertype='IPv4', protocol='tcp',
+                    port_range_min=22, port_range_max=22)
+    return cmgr.qsvc('security-group-rule-create',
+                     security_group_id,
+                     tenant_id=tenant_id, **ssh_rule)
+
+
+def create_security_group_icmp_rule(cmgr, security_group_id,
+                                    tenant_id=None):
+    icmp_rule = dict(direction='ingress',
+                     ethertype='IPv4', protocol='icmp')
+    return cmgr.qsvc('security-group-rule-create',
+                     security_group_id,
+                     tenant_id=tenant_id, **icmp_rule)
+
+
+def create_router_and_add_interfaces(cmgr, name, net_list, **kwargs):
+    tenant_id = kwargs.pop('tenant_id', None)
     name = name or data_utils.rand_name('itempz-r')
-    router = cmgr.qsvc('router-create', name)
+    router = cmgr.qsvc('router-create', name, tenant_id=tenant_id)
     cmgr.qsvc('router-gateway-set', router['id'],
               get_public_network_id(cmgr))
     for network, subnet in net_list:
@@ -11,6 +48,9 @@ def create_router_and_add_interfaces(cmgr, name, net_list):
     return router
 
 
+# for security_group_name_or_id, better using id
+# server_craete does not honor tenant_id, so you need to use the cmgr that
+# will be the owner of the server
 def create_server_on_network(cmgr, network_id, server_name=None,
                              image_id=None, flavor_id=1,
                              security_group_name_or_id=None,
@@ -31,27 +71,163 @@ def create_server_on_network(cmgr, network_id, server_name=None,
 
 
 def create_floatingip_for_server(cmgr, server,
-                                 public_network_id=None, **kwargs):
+                                 public_network_id=None,
+                                 port_id=None,
+                                 **kwargs):
+    if type(server) is not dict:
+        server = cmgr.nova('server-show', server)
+    server_id = server['id']
     public_network_id = public_network_id or get_public_network_id(cmgr)
-    result = UQ.create_floatingip_for_server(
-        cmgr.manager, public_network_id, server['id'])
-    return result
+    if port_id:
+        ip4 = None
+    else:
+        port_id, ip4 = get_port_id_ipv4_of_server(cmgr, server_id)
+    floatingip = cmgr.qsvc('floatingip-create', public_network_id,
+                           port_id=port_id, fixed_ip_address=ip4,
+                           **kwargs)
+    return floatingip
 
 
-def get_public_network_id(cmgr):
-    pub_net = cmgr.qsvc('net-external-list')[0]
-    return pub_net['id']
+def create_ssh_client(public_ip,
+                      username='cirros', password='cubswin:)',
+                      **kwargs):
+    ssh_client = remote_client.RemoteClient(public_ip,
+                                            username, password)
+    return ssh_client
 
 
-def get_flavor_id(cmgr, flavor=None):
+def get_port_id_ipv4_of_server(cmgr, server_id,
+                               ip_addr=None, **kwargs):
+    ports = cmgr.qsvc('port-list',
+                      device_id=server_id, fixed_ip=ip_addr)
+    # TODO(akang): assume only ONE from ports match server_id
+    #              need to handle server given 1+ network.
+    port0 = ports[0]
+    for ip4 in port0['fixed_ips']:
+        ip = ip4['ip_address']
+        if netaddr.valid_ipv4(ip):
+            return (port0['id'], ip)
+
+
+def get_server_fixedips(cmgr, server):
+    fip_list = []
+    if type(server) is not dict:
+        server = cmgr.nova('server-show', server)
+    for if_name, if_addresses in server['addresses'].items():
+        for addr in if_addresses:
+            if ('OS-EXT-IPS:type' in addr and
+                        addr['OS-EXT-IPS:type'] == 'fixed'):
+                fip_list.append(addr['addr'])
+    return fip_list
+
+
+def get_server_floatingips(cmgr, server):
+    fips_list = []
+    if type(server) is not dict:
+        server = cmgr.nova('server-show', server)
+    for if_name, if_addresses in server['addresses'].items():
+        for addr in if_addresses:
+            if ('OS-EXT-IPS:type' in addr and
+                        addr['OS-EXT-IPS:type'] == 'floating'):
+                fips = cmgr.qsvc('floatingip-list',
+                                 floating_ip_address=addr['addr'])
+                if len(fips) > 0:
+                    fips_list.append(fips)
+    return fips_list
+
+
+def delete_server_floatingips(cmgr, server):
+    if type(server) is not dict:
+        server = cmgr.nova('server-show', server)
+    for fips in get_server_floatingips(cmgr, server):
+        for fip in fips:
+            cmgr.qsvc('floatingip_disassociate', fip['id'])
+            cmgr.qsvc('floatingip-delete', fip['id'])
+
+
+def delete_tenant_servers(cmgr, tenant_id=None, wait_for_termination=True,
+                          **kwargs):
+    server_id_list = []
+    for server in cmgr.nova('server-list', detail=True):
+        if tenant_id and server['tenant_id'] != tenant_id:
+            # this works only cmgr as admin priveledge
+            continue
+        delete_server_floatingips(cmgr, server)
+        cmgr.nova('server-delete', server['id'])
+        server_id_list.append(server['id'])
+    if wait_for_termination:
+        for server_id in server_id_list:
+            waiters.wait_for_server_termination(
+                cmgr.manager.servers_client, server_id)
+    else:
+        return server_id_list
+
+
+def delete_router_by_id(cmgr, router_id, **kwargs):
+    routers = cmgr.qscv('router-list', id=router_id)
+    if len(routers) != 1:
+        return None
+    return delete_this_router(cmgr, routers[0])
+
+
+def delete_this_router(cmgr, router):
+    router_id = router['id']
+    cmgr.qsvc('router-delete-extra-routes', router_id)
+    cmgr.qsvc('router-gateway-clear', router_id)
+    ports = cmgr.qsvc('router-port-list', router_id)
+    for port in ports:
+        if port['device_owner'].find(':router_interface') > 0:
+            if 'fixed_ips' in port:
+                for fixed_ips in port['fixed_ips']:
+                    if 'subnet_id' in fixed_ips:
+                        cmgr.qsvc('router-interface-delete',
+                                  router_id,
+                                  fixed_ips['subnet_id'])
+    return cmgr.qsvc('router-delete', router_id)
+
+
+# cmgr with admin privilege may cause resources deleted you may not want!
+def destroy_all_resources(cmgr, **kwargs):
+    this_tenant_id = cmgr.manager.credentials.tenant_id
+    tenant_id = kwargs.pop('tenant_id', this_tenant_id)
+    filters = {'tenant_id': tenant_id}
+    delete_tenant_servers(cmgr, **filters)
+    # rm routers
+    routers = cmgr.qsvc('router-list', **filters)
+    for router in routers:
+        if router['tenant_id']:
+            delete_this_router(cmgr, router)
+    # rm networks/subnets
+    for network in cmgr.qsvc('net-list', **filters):
+        cmgr.qsvc('net-delete', network['id'])
+    # rm security-groups
+    for sg in cmgr.qsvc('security-group-list', **filters):
+        if sg['name'] != 'default':
+            cmgr.qsvc('security-group-delete', sg['id'])
+
+
+def get_public_network_id(cmgr, net_name=None):
+    pub_net_list = cmgr.qsvc('net-external-list')
+    if net_name:
+        for pub_net in pub_net_list:
+            if pub_net['name'] == net_name:
+                return pub_net['id']
+    else:
+        return pub_net_list[0]['id']
+
+
+def get_flavor_id(cmgr, flavor=None, image_name=None):
     if type(flavor) is int:
         return flavor
-    if type(flavor) in (str, unicode) and flavor.isdigit():
-        return int(flavor)
-    for f in cmgr.nova('flavor-list'):
-        if f['name'].find(flavor) >= 0:
-            return int(f['id'])
-    return 1
+    if type(flavor) in (str, unicode):
+        if flavor.isdigit():
+            return int(flavor)
+        for f in cmgr.nova('flavor-list'):
+            if f['name'].find(flavor) >= 0:
+                return int(f['id'])
+    if image_name and image_name.find('cirros') >= 0:
+        return 1
+    return 2
 
 
 def get_image_id(cmgr, image_name=None):
@@ -64,4 +240,3 @@ def get_image_id(cmgr, image_name=None):
         if image['name'].find(image_name) >= 0:
             return image['id']
     return None
-
