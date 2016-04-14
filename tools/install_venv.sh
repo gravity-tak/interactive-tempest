46a7582c543a886249442534373985b3c5e953c5
Alex Kang <akang@vmware.com>
2016-04-14 14:37:13 -0700

    replace tempest/tools/install_venv.py  which is being removed at mitaka release tempest

--- tools/install_venv.sh
+++ tools/install_venv.sh
@@ -5,10 +5,14 @@ venv=${VENV:-.venv}
 with_venv=tools/with_venv.sh
 installvenvopts="--no-site-packages"
 wrapper="${with_venv}"
+tempest_home_dir=""
 
 while [ $# -gt 0 ]; do
   case "$1" in
     -u|--update) update=1;;
+    -t|--tempest-home*|--tempest-dir*)
+        shift;
+        tempest_home_dir=$1;;
     *) testrargs="$testrargs $1";;
   esac
   shift
@@ -39,6 +43,15 @@ function check_iam_at_tempest_dir {
     fi
 }
 
+if [ ! "${tempest_home_dir}" == "" ]; then
+    if [ -d ${tempest_home_dir} ]; then
+        cd ${tempest_home_dir}
+    else
+        echo "tempest directory ${tempest_home_dir} does not exist, or not a directory."
+        exit 1
+    fi
+fi
+
 check_iam_at_tempest_dir
 
 function install_venv {
@@ -54,6 +67,7 @@ function update_venv {
       $venv/bin/pip install -U -r requirements.txt
 }
 
+# install/update VENV
 if [ ! -d $venv ]; then
     install_venv
 else
@@ -63,3 +77,20 @@ else
         update_venv
     fi
 fi
+
+# check vmware-nsx is installed!
+nsx_tempest=$(tools/with_venv.sh pip show vmware-nsx 2>/dev/null | grep vmware-nsx-tempest-plugin)
+echo ""
+if [ "{nsx_tempest}" == "" ]; then
+    echo "--- Warning ---"
+    echo "   vmware-nsx-tempest is not installed at tempest VENV environment."
+    echo "   please goto VENV and install editable vmware-nsx master branch."
+    echo "   Example steps, assume vmware-nsx is /opt/devtest/vmware-nsx:"
+    echo ""
+    echo "     source .venv/bin/active"
+    echo "     pip install -e /opt/devtest/vmware-nsx/"
+else
+    echo "--- OK ---"
+    echo "   You have vmware-nsx-tempest plugin installed."
+fi
+echo ""
