7e6df3f73dcd92f26bae70801f8e1a2db623c00b
Alex Kang <akang@vmware.com>
2016-05-18 14:47:55 -0700

    init release of qos policies client

---
+++ itempest/services/qos/policies.py
@@ -0,0 +1,55 @@
+from tempest.lib.services.network import base
+
+
+class PoliciesClient(base.BaseNetworkClient):
+    resource = 'policy'
+    resource_plural = 'policies'
+    path = 'qos/policies'
+    resource_base_path = '/%s' % path
+    resource_object_path = '/%s/%%s' % path
+
+    def create_policy(self, **kwargs):
+        uri = self.resource_base_path
+        post_data = {self.resource: kwargs}
+        return self.create_resource(uri, post_data)
+
+    def update_policy(self, policy_id, **kwargs):
+        uri = self.resource_object_path % policy_id
+        post_data = {self.resource: kwargs}
+        return self.update_resource(uri, post_data)
+
+    def show_policy(self, policy_id, **fields):
+        uri = self.resource_object_path % policy_id
+        return self.show_resource(uri, **fields)
+
+    def delete_policy(self, policy_id):
+        uri = self.resource_object_path % policy_id
+        return self.delete_resource(uri)
+
+    def list_policies(self, **filters):
+        uri = self.resource_base_path
+        return self.list_resources(uri, **filters)
+
+
+def get_client(client_mgr):
+    """create a qos policies client from manager or networks_client
+
+    For itempest user:
+        from itempest import load_our_solar_system as osn
+        from vmware_nsx_tempest.services.qos import policies_client
+        client = policies_client.get_client(osn.adm.manager)
+    For tempest user:
+        client = policies_client.get_client(osn.adm)
+    """
+    manager = getattr(client_mgr, 'manager', client_mgr)
+    net_client = getattr(manager, 'networks_client')
+    try:
+        _params = manager.default_params_with_timeout_values.copy()
+    except Exception:
+        _params = {}
+    client = PoliciesClient(net_client.auth_provider,
+                            net_client.service,
+                            net_client.region,
+                            net_client.endpoint_type,
+                            **_params)
+    return client
