7ef23a66df912de07ebeec3545f4124b28172f5e
Alex Alex Kang <akang@vmware.com>
2016-05-19 14:59:49 -0700

    qos rule_types client

--- itempest/services/qos/base_qos.py
+++ itempest/services/qos/base_qos.py
@@ -2,13 +2,14 @@ from itempest.services.qos import policies_client
 from itempest.services.qos import \
     bandwidth_limit_rules_client as bandwidth_client
 from itempest.services.qos import dscp_marking_rules_client as dscp_client
-
+from itempest.services.qos import rule_types_client as types_client
 
 class BaseQosClient(object):
     def __init__(self, manager):
         self.policies_client = policies_client.get_client(manager)
-        self.bandwidth_client = bandwidth_client.get_client(manager)
-        self.dscp_client = dscp_client.get_client(manager)
+        self.bandwidths_client = bandwidth_client.get_client(manager)
+        self.dscps_client = dscp_client.get_client(manager)
+        self.types_client = types_client.get_client(manager)
 
     def create_qos_policies(self, name, description, shared, **kwargs):
         result = self.policies_client.create_policy(
@@ -36,44 +37,49 @@ class BaseQosClient(object):
         return result
 
     def create_bandwidth_limit_rule(self, policy_id, **kwargs):
-        result = self.bandwidth_client.create_bandwith_limit_rule(policy_id,
-                                                                  **kwargs)
+        result = self.bandwidths_client.create_bandwith_limit_rule(policy_id,
+                                                                   **kwargs)
         return result
 
     def delete_bandwidth_limit_rule(self, policy_id):
-        result = self.bandwidth_client.delete_bandwidth_limit_rule(policy_id)
+        result = self.bandwidths_client.delete_bandwidth_limit_rule(policy_id)
         return result
 
     def update_bandwidth_limit_rule(self, policy_id):
-        result = self.bandwidth_client.update_bandwidth_limit_rule(policy_id)
+        result = self.bandwidths_client.update_bandwidth_limit_rule(policy_id)
         return result
 
     def list_bandwidth_limit_rules(self, policy_id, **filter):
-        result = self.bandwidth_client.list_bandwidth_limit_rules(policy_id,
-                                                                  **filter)
+        result = self.bandwidths_client.list_bandwidth_limit_rules(policy_id,
+                                                                   **filter)
         return result
 
     def show_bandwidth_limit_rule(self, policy_id):
-        result = self.bandwidth_client.show_bandwidth_limit_rule(policy_id)
+        result = self.bandwidths_client.show_bandwidth_limit_rule(policy_id)
         return result
 
     def create_dscp_marking_rule(self, policy_id, **kwargs):
-        result = self.dscp_client.create_dscp_marking_rule(policy_id,
-                                                           **kwargs)
+        result = self.dscps_client.create_dscp_marking_rule(policy_id,
+                                                            **kwargs)
         return result
 
     def delete_dscp_marking_rule(self, policy_id):
-        result = self.dscp_client.delete_dscp_marking_rule(policy_id)
+        result = self.dscps_client.delete_dscp_marking_rule(policy_id)
         return result
 
     def update_dscp_marking_rule(self, policy_id):
-        result = self.dscp_client.update_dscp_marking_rule(policy_id)
+        result = self.dscps_client.update_dscp_marking_rule(policy_id)
         return result
 
     def list_dscp_marking_rules(self, policy_id, **filter):
-        result = self.dscp_client.list_dscp_marking_rules(policy_id, **filter)
+        result = self.dscps_client.list_dscp_marking_rules(policy_id,
+                                                           **filter)
         return result
 
     def show_dscp_marking_rule(self, policy_id):
-        result = self.dscp_client.show_dscp_marking_rule(policy_id)
+        result = self.dscps_client.show_dscp_marking_rule(policy_id)
+        return result
+
+    def list_qos_rule_types(self):
+        result = self.types_client.list_resources()
         return result
---
+++ itempest/services/qos/rule_types_client.py
@@ -0,0 +1,37 @@
+from tempest.lib.services.network import base
+
+
+class RuleTypesClient(base.BaseNetworkClient):
+    resource = 'rule_type'
+    resource_plural = 'rule_types'
+    path = 'qos/rule_types'
+    resource_base_path = '/%s' % path
+    resource_object_path = '/%s/%%s' % path
+
+    def list_rule_types(self):
+        uri = self.resource_base_path
+        return self.list_resources(uri)
+
+
+def get_client(client_mgr):
+    """create a qos rule_types client from manager or networks_client
+
+    For itempest user:
+        from itempest import load_our_solar_system as osn
+        from vmware_nsx_tempest.services.qos import rule_types_client
+        client = rule_types_client.get_client(osn.adm.manager)
+    For tempest user:
+        client = rule_types_client.get_client(osn.adm)
+    """
+    manager = getattr(client_mgr, 'manager', client_mgr)
+    net_client = getattr(manager, 'networks_client')
+    try:
+        _params = manager.default_params_with_timeout_values.copy()
+    except Exception:
+        _params = {}
+    client = RuleTypesClient(net_client.auth_provider,
+                             net_client.service,
+                             net_client.region,
+                             net_client.endpoint_type,
+                             **_params)
+    return client
