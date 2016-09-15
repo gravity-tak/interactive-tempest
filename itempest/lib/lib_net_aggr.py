9c6590637a2561de37a63fdf526ff18f4a210f18
Alex Kang <akang@vmware.com>
2016-09-15 09:44:52 -0700

    change format of FMT_SNET_ADDR

--- itempest/lib/lib_net_aggr.py
+++ itempest/lib/lib_net_aggr.py
@@ -290,7 +290,7 @@ def show_toplogy(cli_mgr, return_topo=False, prefix=None,
     FMT_X_ROUT = "%sroutes: {routes}" % (' ' * (5 + 4))
     FMT_INTERFACE = "%s>> interface: {name} {id}" % (' ' * 6)
     # FMT_SUBNETS = "%s subnets: {subnets}" % (' ' * 8)
-    FMT_SNET_ADDR = "%s subnet: cidr={cidr} gw={gateway_ip} {name}" % (
+    FMT_SNET_ADDR = "%s subnet: {id} {name} cidr={cidr} gw={gateway_ip}" % (
         ' ' * 8)
     FMT_SERVER = "%s>> server: {name} {id}" % (' ' * 10)
     FMT_SERV_ADDR = "%s>> network: %s "
