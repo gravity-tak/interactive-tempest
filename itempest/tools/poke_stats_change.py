51c7435011915d7bf707783ed0379c284fb3e3f9
Gravity Tak <gravity.tak@gmail.com>
2016-09-24 21:35:19 -0700

    poke_stats_change() to monitor how-long security_goup rule stats updated

--- itempest/tools/poke_stats_change.py
+++ itempest/tools/poke_stats_change.py
@@ -4,18 +4,18 @@ from itempest.lib import utils
 
 
 def m_stats_change(nsxt_client, sect_id, rule_id, nsx_stats,
-                   venus_stats=None, interval=2.5, poke_count=1000):
+                   venus_stats=None, interval=2.5, poke_count=300):
     t0 = time.time()
     for x in range(poke_count):
         ss = nsxt_client.get_firewall_section_rule_stats(sect_id, rule_id)
+        ss.pop('_schema', None)
+        ss.pop('rule_id', None)
+        utils.log_msg(str(ss), 'OS-Interval')
         if ss.get('session_count') != nsx_stats.get('session_count'):
             e_time = (time.time() - t0)
-            ss.pop('u_schema', None)
             msg = "Take %d seconds for STATS to be updated" % int(e_time)
             utils.log_msg(msg, 'OS-Interval')
             return e_time
-        ss.pop('u_schema', None)
-        utils.log_msg(str(ss), 'OS-Interval')
         time.sleep(interval)
     tt = (time.time() - t0)
     raise Exception("After %d seconds, stats do not change!" % int(tt))
@@ -26,8 +26,8 @@ def m_stats_change(nsxt_client, sect_id, rule_id, nsx_stats,
 # venus = osn.get_mcli('Venus')
 # poke_http_stats_change(venus, nsx, venus_segroup_id, 'venus-lb2-http', '172.24.4.6')
 def poke_http_stats_change(cmgr, nsxt_client, os_security_group_id,
-                           lb2_name, web_ip, interval=2.5, poke_count=1000):
-    # os_security_group_id= "35d23271-f317-464a-8456-60ff3387e15a"
+                           lb2_name, web_ip, interval=2.5, poke_count=300):
+    # os_security_group_id = "35d23271-f317-464a-8456-60ff3387e15a"
     # lb2_name = 'venus-lb2-http'
     _sgroup = cmgr.qsvc('security-group-show', os_security_group_id)
     sg_rules = _sgroup['security_group_rules']
